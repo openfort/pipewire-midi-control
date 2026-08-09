fe40ec48e194c9a0611aac7dc824ea04dd808801
openfort <janschwertfeger@hotmail.ch>
2026-08-09 22:08:07 +0200

    use ddcutil cli

--- midipwvol/ddcutil_service.py
+++ midipwvol/ddcutil_service.py
@@ -1,105 +1,49 @@
-from collections import defaultdict
-from dataclasses import dataclass
-from threading import Lock, Timer
+import subprocess
+import re
 
-# pip install sdbus
-# https://python-sdbus.readthedocs.io/en/latest/general.html
-from sdbus import DbusInterfaceCommon, dbus_method, dbus_property
-from sdbus import sd_bus_open, set_default_bus
-from sdbus.exceptions import SdBusUnmappedMessageError
-# If sdbus doesn't work well, maybe worth trying the alternatives listed at:
-# https://gitlab.freedesktop.org/dbus/dbus-python/
+class DdcutilService:
+    """Control monitor brightness via ddcutil CLI"""
 
+    def __init__(self):
+        self.vcp_code = "0x10"  # Brightness
 
-@dataclass
-class DisplayValues:
-    brightness: int | None = None
-    contrast: int | None = None
-
-
-# See: https://github.com/digitaltrails/ddcutil-service/blob/master/ddcutil-service.c
-class DdcutilInterface(DbusInterfaceCommon, interface_name="com.ddcutil.DdcutilInterface"):
-    def __init__(self, *args, **kwargs):
-        # Used for debouncing.
-        # Calling ddcutil-service is slow, we should throttle/debounce the calls to it.
-        self.timer_thread = None
-        self.timer_lock = Lock()
-        self.future_values = defaultdict(DisplayValues)
-
-        super().__init__(*args, **kwargs)
-
-    def _auto_reconnect(self):
-        # This method is a hack.
-        # It's using private attributes and may break on any sdbus update.
+    def get_brightness(self, display_num=1):
+        """Get current brightness (0-100)"""
         try:
-            self.dbus_ping()
-            print("dbus_ping ok")
-        except SdBusUnmappedMessageError:
-            print("dbus_ping failed")
-            new_bus = sd_bus_open()
-            self._dbus.attached_bus = new_bus
-            set_default_bus(new_bus)
+            result = subprocess.run(
+                ["ddcutil", "getvcp", self.vcp_code, "--display", str(display_num)],
+                capture_output=True,
+                text=True,
+                check=True
+            )
+            # Parse output like: "current value =    30, max value =   100"
+            match = re.search(r"current value\s*=\s*(\d+)", result.stdout)
+            if match:
+                return int(match.group(1))
+            return None
+        except subprocess.CalledProcessError as e:
+            print(f"Error getting brightness: {e.stderr}")
+            return None
 
-    @dbus_method("u")
-    def Detect(self, flags:int) -> tuple[int, list[tuple], int, str]:
-        # Returns:
-        # * number_of_displays (signed 32-bit integer)
-        # * detected_displayes (array of a struct of many elements)
-        # * error_status (signed 32-bit integer)
-        # * error_message (string)
-        raise NotImplementedError
 
-    # TODO: Other methods, such as:
-    # * GetVcp
-    # * GetMultipleVcp
-    # * SetVcpWithContext
-    # * GetDisplayState (probably useless)
-    # 
-    # Possibly some properties, such as:
-    # * DdcutilVersion
-    # * ServiceInterfaceVersion
-    # * AttributesReturnedByDetect
-    # * ServiceEmitConnectivitySignals
-    # * StatusValues
-    #
-    # TODO: Some signals, but they require the async interface (insted of the blocking interface):
-    # * ConnectedDisplaysChanged
-    # * VcpValueChanged signal
+    def set_brightness(self, value, display_num=1):
+        """Set brightness (0-100)"""
+        try:
+            subprocess.run(
+                ["ddcutil", "setvcp", self.vcp_code, str(int(value)), "--display", str(display_num)],
+                check=True,
+                capture_output=True
+            )
+            return True
+        except subprocess.CalledProcessError as e:
+            print(f"Error setting brightness: {e.stderr}")
+            return False
 
-    @dbus_method("isyqu")
-    def SetVcp(self, display:int, edid_txt:str, vcp_code:int, vcp_new_value:int, flags:int) -> tuple[int, str]:
-        # Returns:
-        # * error_status (signed 32-bit integer)
-        # * error_message (string)
-        raise NotImplementedError
+    def normalize_midi_to_brightness(self, midi_value):
+        """Convert MIDI 0-127 to brightness 0-100"""
+        return int((midi_value / 127) * 100)
 
-    def set_brightness_contrast(self, displays:list[int], brightness:float=None, contrast:float=None, wait=0.25):
-        # brightness/contrast are in the 0.0 to 1.0 range.
-        # Schedules the display/brightness/contrast values to be updated by the timer thread.
-        if self.timer_thread:
-            self.timer_thread.cancel()
-        with self.timer_lock:
-            for display in displays:
-                if brightness is not None:
-                    self.future_values[display].brightness = brightness
-                if contrast is not None:
-                    self.future_values[display].contrast = contrast
-        self.timer_thread = Timer(wait, self._set_brightness_contrast)
-        self.timer_thread.start()
-
-    def _set_brightness_contrast(self):
-        # Runs in a Timer thread (after a short delay).
-        actions = []
-
-        with self.timer_lock:
-            for display, values in list(self.future_values.items()):
-                del self.future_values[display]
-                if (v := values.brightness) is not None:
-                    actions.append((display, 0x10, round(100 * v)))
-                if (v := values.contrast) is not None:
-                    actions.append((display, 0x12, round(100 * v)))
-
-        print(repr(actions))
-        self._auto_reconnect()
-        for (display, code, value) in actions:
-            self.SetVcp(display, "", code, value, 0)
+    def set_brightness_from_midi(self, midi_value, display_num=1):
+        """Set brightness from MIDI value (0-127)"""
+        brightness = self.normalize_midi_to_brightness(midi_value)
+        return self.set_brightness(brightness, display_num)

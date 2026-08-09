cebb18ae90ec9380fe7618345a98dc08db1dda20
openfort <janschwertfeger@hotmail.ch>
2026-08-09 22:03:48 +0200

    new config

--- midipwvolconfig.py
+++ midipwvolconfig.py
@@ -3,41 +3,39 @@
 
 
 from midipwvol.utils import interp
-
+import subprocess
 
 def handle_midi_message(port, message, pw, ddc):
+    # Programs
     if message.is_cc(0):
-        pw(type="Node", node_description="Built-in Audio Analog Stereo", is_audio=True, is_sink=True).set_volume(message.value / 127)
+        pw(type="Node", node_description="HyperX 7.1 Audio Analog Stereo", is_audio=True, is_sink=True).set_volume(message.value / 127)
     elif message.is_cc(1):
-        pw(type="Node", node_description="Built-in Audio Digital Stereo (HDMI)", is_audio=True, is_sink=True).set_volume(message.value / 127)
-        #pw(type="Node", node_description="Built-in Audio Digital Stereo (HDMI 2)", is_audio=True, is_sink=True).set_volume(message.value / 127)
-    elif message.is_cc(2):
-        pw(type="Node", media_name="Aeropex by AfterShokz", is_audio=True, is_sink=True).set_volume(message.value / 127)
+        subprocess.run(['playerctl', '--player=spotify', 'volume', f'{(message.value/127):.2}'])
+        #pw(type="Node", node_name="spotify", is_audio=True, is_source=True).set_volume(message.value / 127)
     elif message.is_cc(3):
-        pw(type="Node", node_description={"Logitech USB Microphone Mono", }, is_audio=True, is_source=True).set_volume(message.value / 127)
-    elif message.is_cc(4):
-        pw(type="Node", media_name="Jellyfin", node_name="Firefox", is_audio=True, is_source=True).set_volume(message.value / 127)
-    elif message.is_cc(5):
-        pass
+        pw(type="Node", node_name="Brave", is_audio=True, is_source=True).set_volume(message.value / 127)
+
+    # Audio Devices
+    elif message.is_cc(8):
+        pw(type="Node", node_description="HyperX 7.1 Audio Analog Stereo", is_audio=True, is_source=True).set_volume(message.value / 127)
+    elif message.is_cc(9):
+        pw(type="Node", node_description="HyperX 7.1 Audio Analog Stereo", is_audio=True, is_sink=True).set_volume(message.value / 127)
+        pw(type="Node", node_description="GA102 High Definition Audio Controller Digital Stereo (HDMI)", is_audio=True, is_sink=True).set_volume(message.value / 127)
+
+    # Media Keys
+    elif message.type == 'note_on' and message.note == 22:
+        subprocess.run(['playerctl', '--player=spotify', 'play-pause'])
+    elif message.type == 'note_on' and message.note == 18:
+        subprocess.run(['playerctl', '--player=spotify', 'previous'])
+    elif message.type == 'note_on' and message.note == 19:
+        subprocess.run(['playerctl', '--player=spotify', 'next'])
+
+    # Display
+    elif message.type == 'note_on' and message.note == 16:
         # Display brightness change:
-        #ddc.set_brightness_contrast([1, 2], brightness=message.value / 127)
-    elif message.is_cc(6):
-        pass
-        # Display contrast change:
-        #ddc.set_brightness_contrast([1, 2], contrast=message.value / 127)
-    elif message.is_cc(7):
-        # Display combined brightness-contrast change.
-        # Works well for my AOC Q27P1 displays, but probably won't work well for other displays.
-        # Please remember to check if you can see all the shades in a test image like this:
-        # http://www.lagom.nl/lcd-test/contrast.php
-        v = message.value
-        LIMIT = 32  # out of 0~127 range
-        ddc.set_brightness_contrast(
-            displays=[1, 2],
-            # Brightness is zero for values from 0 to 32,
-            # then it increases linearly until 100%.
-            brightness=interp(message.value, [(0, 0.0), (32, 0.0), (127, 1.0)]),
-            # Contrast is linear from 0% to until 50% at value 32,
-            # then it stays constant at 50% for higher values.
-            contrast=interp(message.value, [(0, 0.0), (32, 0.5), (127, 0.5)]),
-        )
+        brightness = ddc.get_brightness(display_num=2)
+        #print(brightness)
+        if ddc.get_brightness(display_num=2) > 50:
+            ddc.set_brightness(0, display_num=2)
+        else:
+            ddc.set_brightness(60, display_num=2)
